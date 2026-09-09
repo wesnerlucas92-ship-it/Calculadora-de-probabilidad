5647c15ef6979d830ac423a3c202c634a9d0a60e
wesnerlucas92-ship-it <wesnerlucas92@gmail.com>
2026-09-09 15:13:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prob-calc-v2';
+const CACHE_NAME = 'prob-calc-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -6,8 +6,25 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  // Fuerza al nuevo service worker a activarse inmediatamente
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  // Borra automáticamente cualquier versión vieja de caché (v1, v1.1, etc.)
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
